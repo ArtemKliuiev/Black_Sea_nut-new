d8dc3650c1f7782dcd4c4efae4f0774bbb77d4a5
ArtemKliuiev <117043590+ArtemKliuiev@users.noreply.github.com>
2023-07-05 16:47:23 +0300

    pagespeed

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const { src, dest, series, parallel, watch } = require("gulp");
 const gulp = require("gulp");
 const autoprefixer = require("gulp-autoprefixer");
 const cssbeautify = require("gulp-cssbeautify");
-const removeComments = require('gulp-strip-css-comments');
+const removeComments = require("gulp-strip-css-comments");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
 const cssnano = require("gulp-cssnano");
@@ -13,41 +13,48 @@ const plumber = require("gulp-plumber");
 const panini = require("panini");
 const del = require("del");
 const notify = require("gulp-notify");
-const webpack = require('webpack');
-const webpackStream = require('webpack-stream');
+const webpack = require("webpack");
+const webpackStream = require("webpack-stream");
 const browserSync = require("browser-sync").create();
 const webp = require("gulp-webp");
+const htmlmin = require("gulp-htmlmin"); // Добавленный плагин
 
 /* Paths */
-const srcPath = 'src/';
-const distPath = 'dist/';
+const srcPath = "src/";
+const distPath = "dist/";
 
 const path = {
   build: {
-    html: 'dist/',
-    js: 'dist/assets/js/',
-    css: 'dist/assets/css/',
-    images: 'dist/assets/images/',
-    fonts: 'dist/assets/fonts/',
-    videos: 'dist/assets/video/'
+    html: "dist/",
+    js: "dist/assets/js/",
+    css: "dist/assets/css/",
+    images: "dist/assets/images/",
+    fonts: "dist/assets/fonts/",
+    videos: "dist/assets/video/",
   },
   src: {
-    html: ['src/*.html', 'src/personal-area/**/*.html', 'src/basket/**/*.html'],
-    js: 'src/assets/js/*.js',
-    css: 'src/assets/scss/**/*.scss',
-    images: 'src/assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
-    fonts: 'src/assets/fonts/**/*',
-    videos: 'src/assets/video/**/*'
+    html: [
+      "src/*.html",
+      "src/personal-area/**/*.html",
+      "src/basket/**/*.html",
+    ],
+    js: "src/assets/js/*.js",
+    css: "src/assets/scss/**/*.scss",
+    images:
+      "src/assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    fonts: "src/assets/fonts/**/*",
+    videos: "src/assets/video/**/*",
   },
   watch: {
-    html: 'src/**/*.html',
-    js: 'src/assets/js/**/*.js',
-    css: 'src/assets/scss/**/*.scss',
-    images: 'src/assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
-    fonts: 'src/assets/fonts/**/*',
-    videos: 'src/assets/video/**/*'
+    html: "src/**/*.html",
+    js: "src/assets/js/**/*.js",
+    css: "src/assets/scss/**/*.scss",
+    images:
+      "src/assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    fonts: "src/assets/fonts/**/*",
+    videos: "src/assets/video/**/*",
   },
-  clean: './dist/'
+  clean: "./dist/",
 };
 
 /* Tasks */
@@ -62,8 +69,8 @@ function convertToWebP() {
 function serve() {
   browserSync.init({
     server: {
-      baseDir: "./" + distPath
-    }
+      baseDir: "./" + distPath,
+    },
   });
 }
 
@@ -84,63 +91,67 @@ function html() {
 
 function css() {
   return src(path.src.css, { base: srcPath + "assets/scss/" })
-    .pipe(plumber({
-      errorHandler: function (err) {
-        notify.onError({
-          title: "SCSS Error",
-          message: "Error: <%= error.message %>"
-        })(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(sass({
-      includePaths: './node_modules/'
-    }))
-    .pipe(autoprefixer({
-      cascade: true
-    }))
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: "SCSS Error",
+            message: "Error: <%= error.message %>",
+          })(err);
+          this.emit("end");
+        },
+      })
+    )
+    .pipe(sass({ includePaths: "./node_modules/" }))
+    .pipe(autoprefixer({ cascade: true }))
     .pipe(cssbeautify())
     .pipe(dest(path.build.css))
-    .pipe(cssnano({
-      zindex: false,
-      discardComments: {
-        removeAll: true
-      }
-    }))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
     .pipe(removeComments())
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".css"
-    }))
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".css",
+      })
+    )
     .pipe(dest(path.build.css))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function cssWatch() {
   return src(path.src.css, { base: srcPath + "assets/scss/" })
-    .pipe(plumber({
-      errorHandler: function (err) {
-        notify.onError({
-          title: "SCSS Error",
-          message: "Error: <%= error.message %>"
-        })(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(sass({
-      includePaths: './node_modules/'
-    }))
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".css"
-    }))
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: "SCSS Error",
+            message: "Error: <%= error.message %>",
+          })(err);
+          this.emit("end");
+        },
+      })
+    )
+    .pipe(sass({ includePaths: "./node_modules/" }))
+    .pipe(
+      rename({
+        suffix: ".min",
+        extname: ".css",
+      })
+    )
     .pipe(dest(path.build.css))
     .pipe(browserSync.reload({ stream: true }))
     .on("end", browserSync.reload);
 }
 
 function js() {
-  return src(path.src.js, { base: srcPath + 'assets/js/' })
+  return src(path.src.js, { base: srcPath + "assets/js/" })
     .pipe(uglify())
     .pipe(dest(path.build.js))
     .pipe(browserSync.reload({ stream: true }));
@@ -181,7 +192,18 @@ function watchFiles() {
   watch([path.watch.videos], videos);
 }
 
-const build = series(clean, parallel(html, css, js, images, fonts, videos));
+function htmlMinify() {
+  return src(path.build.html + "*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest(path.build.html))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+const build = series(
+  clean,
+  parallel(html, css, js, images, fonts, videos),
+  htmlMinify
+);
 const dev = parallel(build, watchFiles, serve);
 
 /* Exports Tasks */
@@ -195,3 +217,4 @@ exports.clean = clean;
 exports.build = build;
 exports.default = dev;
 exports.convertToWebP = convertToWebP;
+exports.htmlMinify = htmlMinify;
